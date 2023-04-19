7f0e281eb593aff331c708ca43275b23945c6a16
Eli VanderBilt <40370237+elimvb@users.noreply.github.com>
2023-04-19 11:00:23 -0700

    Added basic scene for testing deterministic AB robot motion

---
+++ ArmRobot/Assets/Scripts/TestABController_Rails_Basic.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class TestABController_Rails_Basic : MonoBehaviour
+{
+    public GameObject forceTarget = null;
+
+    [SerializeField]
+    [Header("Control with Keyboard or Action")]
+    ABControlMode controlMode = ABControlMode.Keyboard_Input;
+
+    [Header("Reference to this Articulation Body")]
+    public ArticulationBody ab;
+
+    [Header("Speed for movement and rotation")]
+    public float moveSpeed;
+    public float rotateSpeed;
+    private float move;
+    private float look;
+    private float newCOM;
+
+    
+    public void OnMove(InputAction.CallbackContext context) 
+    {
+        if(controlMode != ABControlMode.Keyboard_Input) 
+        {
+            return;
+        }
+
+        move = context.ReadValue<float>();
+    }
+
+    public void OnRotate(InputAction.CallbackContext context)
+    {
+        if(controlMode != ABControlMode.Keyboard_Input) 
+        {
+            return;
+        }
+
+        look = context.ReadValue<float>();
+    }
+
+
+    private void FixedUpdate() 
+    {
+        Move();
+        Rotate();
+    }
+    void Move()
+    {
+        //find target velocity
+        Vector3 currentVelocity = ab.velocity;
+        Vector3 targetvelocity = new Vector3(0, 0, move);
+        targetvelocity *= moveSpeed;
+
+        //allign direction
+        targetvelocity = ab.transform.TransformDirection(targetvelocity);
+
+        //calculate forces
+        Vector3 velocityChange = (targetvelocity - currentVelocity);
+
+        //ab.AddForce(velocityChange);
+        Vector3 forcePosition = new Vector3();
+        if(forceTarget != null)
+        {
+            forcePosition = forceTarget.transform.position;
+        }
+
+        else
+        {
+            forcePosition = ab.worldCenterOfMass;
+        }
+        // Debug.Log("Applying force of " + velocityChange);
+        ab.centerOfMass = Vector3.zero;
+        Debug.Log("New center of mass is " + ab.centerOfMass);
+        ab.AddForceAtPosition(velocityChange, forcePosition);
+    }
+
+    void Rotate()
+    {
+        //note 2021 unity adds a forceMode paramater to AddTorque so uhhhh
+        ab.AddTorque(Vector3.up * look * rotateSpeed);
+    }
+
+}
